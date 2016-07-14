5d88ef0a8eaee8f39c252d4fe3a286e0a08e16dc
diligentdpf <1852300415@qq.com>
2016-07-14 11:52:08 +0800

    初次提交
    初次提交

---
+++ src/helloAnt/HelloAnt.java
@@ -0,0 +1,13 @@
+package helloAnt;
+
+public class HelloAnt {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("hello ant!");
+  }
+
+}
